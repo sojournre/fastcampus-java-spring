fb7851d42c482536fdf20e27fcf033c16e288fc8
Jaejin Jeong <sojourner@bliex.com>
2022-02-28 21:55:33 +0900

    apply gitignore *.class

--- Chapter8/src/ch02/Solution4.java
+++ Chapter8/src/ch02/Solution4.java
@@ -1,6 +1,11 @@
 package ch02;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
 
 public class Solution4 {
     public static int solution(int[] A, int[] B) {
@@ -20,13 +25,27 @@ public class Solution4 {
         return answer;
     }
 
-    public static void main(String[] args) {
-        int[] A = {2,2,2,2}; //1 3 5 7
-        int[] B = {1,1,1,1}; //2 2 6 8
+    public static void main(String[] args) throws ParseException {
+        int[] A = {1,3,5,7}; //1 3 5 7
+        int[] B = {2,2,6,8}; //2 2 6 8
+
         // result 3
 
+        String[][] C= {{"09:00", "kim"}, {"09:05", "bae"}};
         int sol = solution(A, B);
         System.out.println("sol = " + sol);
+
+
+        System.out.println("C = " + (C[0][0].compareTo(C[1][0])));
+        System.out.println("C = " + (C[1][0].compareTo(C[0][0])));
+
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm");
+        Date to = simpleDateFormat.parse(C[0][0]);
+
+        System.out.println("to = " + to);
+
     }
+
 }
 
