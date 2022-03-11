ed340e6f0279664a2f405a533520bcdb20fe340e
Jaejin <sojournre@gmail.com>
2022-03-12 00:50:21 +0900

    decorator pattern

---
+++ Chapter6/src/ch19/EtiopiaAmericano.java
@@ -0,0 +1,9 @@
+package ch19;
+
+public class EtiopiaAmericano extends Coffee{
+
+    @Override
+    public void brewing() {
+        System.out.print("EtiopiaAmericano ");
+    }
+}
---
+++ Chapter6/src/ch19/KenyaAmericano.java
@@ -0,0 +1,8 @@
+package ch19;
+
+public class KenyaAmericano extends Coffee{
+    @Override
+    public void brewing() {
+        System.out.print("KenyaAmericano ");
+    }
+}
